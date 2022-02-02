0164c5e88c21b68685151d0639d43b6bd19e2d59
jpquinn66 <G00411303@gmit.ie>
2022-02-02 21:17:42 +0000

    week03

---
+++ nameAge.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+print('Hello world!')
+print ('What is your name?')
+myName = input()
+print ('it is good to meet you, ' + myName)
+print ('the length of your name is:')
+print(len(myName))
+print ('What age are you?')   # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge)+1)+ ' in a year')
